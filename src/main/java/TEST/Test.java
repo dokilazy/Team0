481b4e3a54041213c8dd05df18b0e5f08a017ffb
Nguyen Dang Vu <nguyendangvu89@gmail.com>
2022-07-07 16:20:04 +0700

    update test
    add new method verify ABC

--- src/main/java/TEST/Test.java
+++ src/main/java/TEST/Test.java
@@ -1,32 +1,33 @@
 package TEST;
 
 public class Test {
-
 	
 	public void TC_H1_verify_page_title() {
 
 	
 	}
 	
-	
 	public void TC_H2_verify_lastest_articles_items() {
 	
 		
 	}
-	
-	
-	public void TC_H3_verify_() {
-			
+		
+	public void TC_H3_verify_Update_7July() {
+			// Test
 		
 	}
-	
-	
+		
 	public void TC_H4_verify_() {
 				
 		
 	}
 
-	// ----MY Changes  -----
+	public void TC_H5_verify_ABC() {
+				
+		
+	}
+
+
 	
 
 }
